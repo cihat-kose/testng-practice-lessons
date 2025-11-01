5b47bbcf1787985d8202fd9f7013c11688d55d36
Cihat Köse <cihat.kose@hotmail.com>
2025-11-01 16:09:35 +0100

    TestNG Intro

--- src/test/java/_01_TestNGIntro/TestNGIntroductionExample.java
+++ src/test/java/_01_TestNGIntro/TestNGIntroductionExample.java
@@ -5,6 +5,17 @@ import org.testng.annotations.Test;
 public class TestNGIntroductionExample {
 
     // TestNG'nin temellerini anlamak için basit bir test sınıfı oluşturuyoruz.
+    @Test
+    public void testMethod1() {
+        System.out.println("TestNG Method 1 calisti!"); // TestNG metotlarının nasıl çalıştığını gösterir.
+    }
 
-
+    @Test
+    public void testMethod2() {
+        System.out.println("TestNG Method 2 calisti!"); // TestNG'de birden fazla metot oluşturabilir ve sırasıyla çalıştırabilirsiniz.
+    }
 }
+
+
+
+
